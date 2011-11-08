ef5e6b7ffd5dcec167feb779f14bc74f32a408ec
Markus Trapp <maftrapp@gmail.com>
2011-11-07 20:28:29 -0500

    my main function for queues

---
+++ tests/queue_main.c
@@ -0,0 +1,17 @@
+//#include "queues.c"
+#include "rtx.h"
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	
+	//queue1 = (queue*)(malloc(sizeof(queue));
+	
+	//pcb1 = (pcb*)(malloc(sizeof(queue));
+	
+	
+	
+	
+	getchar();
+    return 0;
+}
