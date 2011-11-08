7d284c7dc4f8bc54ee35c0f852212b1e7b307a08
JinSung Kang <gogs14@hotmail.com>
2011-11-07 22:04:40 -0500

    jin-data structures testcase

--- tests/queue_main.c
+++ tests/queue_main.c
@@ -4,14 +4,24 @@
 #include <stdlib.h>
 int main()
 {
+	pcb *test;
+	pcb *test1;
+	pcb *head;
+
+	test = (pcb*) malloc(sizeof(pcb));
+	test1 = (pcb*) malloc(sizeof(pcb));
 	
-	//queue1 = (queue*)(malloc(sizeof(queue));
+	head = test;
+
+	test->priority = 1;
+	test->pid = 1;
+	test->next=test1;
+	test1->priority = 1;
+	test1->pid = 5;
 	
-	//pcb1 = (pcb*)(malloc(sizeof(queue));
-	
-	
-	
-	
-	getchar();
-    return 0;
+	if (head->next->pid == 5)
+		printf ("hellO \n");
+	if (head->pid == 1)
+		printf ("Fuck yeah \n");
+	return 0;
 }
