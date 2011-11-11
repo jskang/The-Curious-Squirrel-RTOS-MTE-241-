6129fc81f5d74a63cd12056b205803c29552c4a3
JinSung Kang <gogs14@hotmail.com>
2011-11-10 22:50:03 -0500

    file moved

---
+++ tests/queue_testing.h
@@ -0,0 +1,23 @@
+/*
+ *  testing functions.h
+ *  main
+ *
+ *  Created by Markus Trapp on 11-11-09.
+ *  Copyright 2011 University Of waterloo. All rights reserved.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "queues.h"
+
+void print_pcb_queue(pcb_queue *Q);
+
+void print_pcb(pcb *to_print);
+
+void print_msg(Msg_Env *to_print);
+
+void print_msg_queue(msg_queue *Q);
+
+void print_msg_all_queue(msg_queue *Q);
+
+void print_rpq_test(pcb_queue *Q[]);
