d2864136f8c34ee9fcd3bf09b6b60bd9c0cfd6e2
JinSung Kang <gogs14@hotmail.com>
2011-11-25 19:21:34 -0500

    roll back

--- init.c
+++ init.c
@@ -26,7 +26,6 @@ int kbd_pid, crt_pid;
 caddr_t kbd_mmap, crt_mmap;
 int bufsize = BUFFERSIZE;
 int kbd_fid, crt_fid, kbd_status , crt_status;
-int wall_clock_flag = 0;
 jmp_buf kernel_buf;
 initialization_table i_table[N_TOTAL_PCB];
 
